67e5949362ce8a31ccc677460da49113d0b43ef1
SHARKZTECH <87173929+SHARKZTECH@users.noreply.github.com>
2022-01-14 17:22:18 +0300

    s

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,12 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <img
+        src="https://cdn4.vectorstock.com/i/1000x1000/50/68/avatar-icon-of-girl-in-a-baseball-cap-vector-16225068.jpg"
+        alt="user"
+        className="img"
+      />
+    </div>
+  );
 };
 export default Header;
--- src/components/InputArea.js
+++ src/components/InputArea.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 const InputArea = () => {
+  const [msg, setMsg] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="inputarea">
-      <form>
+      <form onSubmit={submitHandler}>
         <input
           type="text"
           className="input"
           placeholder="Type your message here.."
+          value={msg}
+          onChange={(e) => setMsg(e.target.value)}
         />
         <button className="btn">send</button>
       </form>
